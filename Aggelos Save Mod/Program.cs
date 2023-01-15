2cbedc1dbc6ef9e8559c456f280ba95dc8aff2af
MegaPatches <ptdrummer2000@yahoo.com>
2023-01-14 22:10:58 -0600

    Renamed Form1 to "MainForm" and window title to "Aggelos Mega Patcher"

--- Aggelos Save Mod/Program.cs
+++ Aggelos Save Mod/Program.cs
@@ -23,7 +23,7 @@ namespace Aggelos_Save_Mod
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 
